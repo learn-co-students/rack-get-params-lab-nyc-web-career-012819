432809e71591ee5274b400faaeb29a2b2116c118
github username <github email address>
2019-02-18 15:05:09 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-  @@cart = @@items
+  @@cart = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -14,7 +14,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart.length == 0
+      if @@cart.empty?
         resp.write "Your cart is empty"
       else
         @@cart.each do |item|
